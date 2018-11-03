0dcddbfdd1f851b6286a4423cc25105ab7bf2968
lethanhson2910 <lethanhson2910@gmail.com>
2018-11-03 23:40:29 +0700

    Add bootstrap

--- resources/views/Edit.blade.php
+++ resources/views/Edit.blade.php
@@ -1,3 +1,8 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
 <!DOCTYPE html>
 @if ($errors->any())
 <div class="alert alert-danger">
@@ -8,42 +13,76 @@
     </ul>
 </div>
 @endif
-<html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- Website CSS style -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Website Font style -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <!-- Google Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
+
+    <title>Edit</title>
 </head>
 
 <body>
-    <div class="col-xs-8 col-xs-offset-2" style="margin-top:50px;">
-        <form class="form-horizontal form-row-seperated" action="{{route('home.user.edit',$nhanvien->id)}}" method="Post">
-            {{csrf_field()}}
-            <div class="form-group">
-                <label for="exampleInputEmail1">Name</label>
-                <input type="text" class="form-control" value="{{$nhanvien->name}}" name="name">
-                <br />
-                <br />
-                <label for="exampleInputEmail1">Email</label>
-                <input type="text" class="form-control" value="{{$nhanvien->email}}" name="email">
-                <br />
-                <br />
-                <label for="exampleInputEmail1">Gender</label>
-                <select name="gender">
-                    <option value="Nam" {{ $nhanvien->gender == 'Nam' ? "selected" : ""  }} >Nam</option>
-                    <option value="Nữ" {{ $nhanvien->gender == 'Nữ' ? "selected" : ""  }}>Nữ</option>
-                </select>
-                <br />
-                <br />
-                <label for="exampleInputEmail1">Udated at</label>
-                <input type="date" class="form-control" value="{{$nhanvien->updated_at}}" name="updated">
+    <div class="container">
+        <div class="row main">
+            <div class="main-login main-center">
+                <form class="" method="post" action="{{route('home.user.edit',$nhanvien->id)}}">
+                    {{csrf_field()}}
+                    <div class="form-group">
+                        <label for="name" class="cols-sm-2 control-label">Name</label>
+                        <div class="cols-sm-10">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                                <input type="text" class="form-control" name="name" id="name" placeholder="Enter your Name" value="{{$nhanvien->name}}" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email" class="cols-sm-2 control-label">Email</label>
+                        <div class="cols-sm-10">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+                                <input type="text" class="form-control" name="email" id="email" placeholder="Enter your Email" value="{{$nhanvien->email}}" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email" class="cols-sm-2 control-label">Gender</label>
+                        <div class="b-select-wrap">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                                <select class="form-control b-select" name="gender">
+                                    <option value="Nam" {{ $nhanvien->gender == 'Nam' ? "selected" : ""  }}>Nam</option>
+                                    <option value="Nữ" {{ $nhanvien->gender == 'Nữ' ? "selected" : ""  }}>Nữ</option>
+                                </select>
+                            </div>
+
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-default">Gửi</button>
+
+
+                </form>
             </div>
-
-
-            <button type="submit" class="btn btn-default">Gửi</button>
-        </form>
+        </div>
     </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- resources/views/List.blade.php
+++ resources/views/List.blade.php
@@ -1,36 +1,42 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<div class="col-xs-8 col-xs-offset-2" style="margin-top:50px;">
-    <table class="table table-hover">
-     <tr>
-         <td>id</td>
-         <td>name</td>
-         <td>email</td>
-         <td>gender</td>
-         <td>Action</td>
-     </tr>
-     @foreach($nhanvien as $nv)
-       <tr>
-         <td>{{$nv->id}}</td>
-         <td>{{$nv->name}}</td>
-         <td>{{$nv->email}}</td>
-           <td>{{$nv->gender}}</td>
-           <td>
-               <a href='{{route('home.user.edit',$nv->id)}}'> Edit</a>
-               <a href='{{route('home.user.delete',$nv->id)}}'> Delete</a>
-           </td>
-       </tr>
-     @endforeach
-
-    </table>
+<div class="container">
+  <h2>List User</h2>
+  <table class="table">
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Gender</th>
+        <th>Action</th>
+      </tr>
+      @foreach($nhanvien as $nv)
+         <tr>
+           <th>{{$nv->id}}</th>
+           <th>{{$nv->name}}</th>
+           <th>{{$nv->email}}</th>
+             <th>{{$nv->gender}}</th>
+             <th>
+                 <a href='{{route('home.user.edit',$nv->id)}}'> Edit</a>
+                 <a href='{{route('home.user.delete',$nv->id)}}'> Delete</a>
+             </th>
+         </tr>
+       @endforeach
+  </table>
 </div>
+<div class="container">
 <a href="{{route('home.user.create')}}"><button type="button" class="btn btn-default">Add</button></a>
+</div>
+
 </body>
 </html>
